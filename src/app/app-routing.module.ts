05775bc71722459d97ce3960e7ff8e80cb83bd24
Will <doggy.huang@gmail.com>
2017-10-29 09:55:59 +0800

    02 練習設定 enableTracing 與 useHash 調整路由機制的特性

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: false,
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
