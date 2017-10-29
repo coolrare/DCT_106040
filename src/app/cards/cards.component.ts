726eb9c08d059bddd369e228e143145b625b1657
Will <doggy.huang@gmail.com>
2017-10-29 14:37:03 +0800

    10 練習透過 ActivatedRoute 服務元件取得路由參數

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cards',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+  type: string;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // this.type = this.route.snapshot.params['type'];
+    this.route.params.subscribe((params) => {
+      this.type = params['type'];
+    });
+  }
+
+  plusOne() {
+    this.router.navigate(['/cards', parseInt(this.type)+1]);
   }
 
 }
