c2f0638179bdfa08fdd7199740fbce4d9feaf6f7
Will <doggy.huang@gmail.com>
2017-10-29 10:29:11 +0800

    示範預設路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
+  { path: '', component: Page1Component },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component }
 ];
