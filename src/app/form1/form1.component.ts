e4068c814093e43b237ccfba166e5a3eac927b8c
Will <doggy.huang@gmail.com>
2017-11-05 11:55:12 +0800

    19 練習 NgForm 與 NgModelGroup 並套用 ngSubmit 事件

--- src/app/form1/form1.component.ts
+++ src/app/form1/form1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form1',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Form1Component implements OnInit {
 
-  data: any = {};
+  data: any = {
+    title: 'Hello',
+    subtitle: '12312312312'
+  };
+
+  doSubmit(form: NgForm) {
+    if (form.valid) {
+      // HTTP POST
+    }
+  }
 
   constructor() { }
 
