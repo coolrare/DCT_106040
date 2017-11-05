b8e2244e141669523aa93fd22a32e1e7eea84766
Will <doggy.huang@gmail.com>
2017-11-05 13:45:33 +0800

    20 練習 Reactive Form 基本開發方式

--- src/app/form2/form2.component.ts
+++ src/app/form2/form2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form2',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Form2Component implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      title: ['Hello', Validators.required],
+      subtitle: 'World'
+    });
   }
 
 }
